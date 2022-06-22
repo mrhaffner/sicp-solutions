a8b0e42b74190d9337b758430c3a0eccc3a14ced
mrhaffner <mrhaffner13@gmail.com>
2022-06-22 13:17:17 -0500

    first commit - solution 1-11

---
+++ README.md
@@ -0,0 +1,5 @@
+These are my solutions to selected problems from the book Structure and Interpretation of Computer Programs (2nd edition) a.k.a. SICP.
+
+My solutions are written in Clojure. The book uses Scheme. These languages are both LISPs - so there is not much of a difference.
+
+The solutions are not meant to be run directly from the solution file. Instead copy and paste them into your REPL.
---
+++ chapter-1/1-11.clj
@@ -0,0 +1,27 @@
+;; Exercise 1.11
+;; page 53
+
+;; Implement the function:
+;;  f(n) = { n if n < 3,
+;;       = { f(n-1) + 2*f(n-2) + 3*f(n-3) if n >= 3.
+
+;; Recursive implementation
+(defn f
+  [n]
+  (if (< n 3)
+    n
+    (+ 
+      (f (- n 1)) 
+      (* 2 (f (- n 2))) 
+      (* 3 (f (- n 3))))))
+
+;; Iterative implementation
+(defn f
+    [n]
+    (f-iter 0 1 2 n))
+
+(defn f-iter
+    [a b c i]
+    (if (<= i 0)
+        a
+        (recur b c (+ (* 3 a) (* 2 b) c) (dec i))))
