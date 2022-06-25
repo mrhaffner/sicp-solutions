72f888cc93d63692d9b6f11a535d48af8a6605d8
mrhaffner <mrhaffner13@gmail.com>
2022-06-25 10:26:21 -0500

    updated comments

--- chapter-1/1-17.clj
+++ chapter-1/1-17.clj
@@ -1,23 +1,35 @@
 ;; Exercise 1.17
-;; page 88
+;; page 60
 
+;; perform multiplication by using halve and double  on two positive numbers
 (defn double
+    "Doubles a number"
     [a]
     (* a 2))
 
 (defn halve
+    "Halve a number"
     [a]
     (/ a 2))
 
 (defn fast-*-inner
+    "Multiplies two positive numbers where at least one number is even.  Operates in logarithmic time."
     [a b]
-    (cond   
-            (= b 1) a
-            (even? b) (fast-* (double a) (halve b))
-            :else (fast-* (+ a b) (dec b))))
+    (if   
+        (= b 1) a
+        (even? b) (recur (double a) (halve b))))
+
+(defn slow-*
+    "Multiplies two positive odd numbers"
+    [a b]
+    (if (= b 0) 0
+        (+ a (slow-* a (dec b)))))
 
 (defn fast-*
+    "Multiply two positive numbers"
     [a b]
-    (if (even? a) (fast-*-inner b a)
-        (fast-*-inner a b)))
+    (cond 
+            (even? a) (fast-*-inner b a)
+            (even? b) (fast-*-inner a b)
+            :else (slow-* a b)))
 
