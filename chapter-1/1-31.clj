d0d428ab3210951f5f54ef4d036a3f93b2cf5c6b
mrhaffner <mrhaffner13@gmail.com>
2022-06-26 19:46:34 -0500

    added solution for 1-31

---
+++ chapter-1/1-31.clj
@@ -0,0 +1,42 @@
+;; Exercise 1.31
+;; page 80-81
+
+;; create a procedure "product" that returns the product of all values generated
+;; from a to b by calling "term" procedure and stepping with the "term" procedure
+(defn product
+    [term a next b]
+    (if (> a b)
+        1
+        (* (term a)
+           (product term (next a) next b))))
+
+;; n!
+(defn factorial
+    [n]
+    (product identity 1 inc n))
+
+(defn pi-term
+    [n]
+    (/ (+ 2
+          (if (even? n) n
+                        (dec n)))
+       (+ 2
+          (if (odd? n) n
+                       (dec n)))))
+
+;; estimate pi using n term
+(defn pi
+    [n]
+    (* 4
+       (product pi-term 1 inc n)))
+
+;; b. Make product as iterative process
+(defn iter
+    [result term a next b]
+    (if (> a b) 
+        result
+        (recur (* result (term a)) term (next a) next b)))
+
+(defn product
+    [term a next b]
+    (iter 1 term a next b))
