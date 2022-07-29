56754f17c5b2680ac713fc4490bc1561f3957589
mrhaffner <mrhaffner13@gmail.com>
2022-07-29 18:57:10 -0500

    added solution for 3.3

---
+++ chapter-3/3.3.clj
@@ -0,0 +1,34 @@
+;; Exercise 3.2
+;; Page 304
+
+;; Modify the make-account procedure so that
+;; it creates password-protected accounts. That is, make-account
+;; should take a symbol as an additional argument, 
+;; Thee resulting account object should process a request only
+;; if it is accompanied by the password with which the account was created, and should otherwise return a complaint
+
+(defn make-account 
+    [initial-balance new-password]
+    (let [balance (atom initial-balance) password new-password]
+        (defn withdraw [amount]
+            (if (>= @balance amount)
+                (swap! balance #(- % amount))
+                "Insufficient funds"))
+        (defn deposit [amount]
+            (swap! balance #(+ % amount)))
+        (defn dispatch [input-password m]
+            (if (= input-password password)
+                (cond 
+                    (= m :withdraw) withdraw
+                    (= m :deposit) deposit
+                    :else (throw (AssertionError. 
+                            (str "Unknown request: MAKE-ACCOUNT " m))))
+                (throw (AssertionError. 
+                            (str "Incorrect password")))))
+        dispatch))
+
+(def a (make-account 10 :1234))
+
+((a :1234 :withdraw) 1)
+
+((a :123 :withdraw) 1)
